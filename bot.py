0fe8279a3e73e2826ed3ea037bae7a053392e6bd
cfeuirt <rifireywan25@gmail.com>
2026-05-19 07:16:56 +0000

    add autorole

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 MOD_FILE = 'mods.json'
+GUILD_ID = 1506035311048917052
+AUTOROLE_NAME = 'Membres'
 
 def load_mods():
     if os.path.exists(MOD_FILE):
@@ -20,7 +22,14 @@ def save_mods(mods):
 
 @bot.event
 async def on_ready():
-    print(f'Bot connecte : {bot.user}')
+    print(f'Bot 1 connecte : {bot.user}')
+
+@bot.event
+async def on_member_join(member):
+    role = discord.utils.get(member.guild.roles, name=AUTOROLE_NAME)
+    if role:
+        await member.add_roles(role)
+        print(f'Role Membres donne a {member.name}')
 
 @bot.command()
 @commands.has_permissions(administrator=True)
@@ -28,7 +37,17 @@ async def setmod(ctx, member: discord.Member, numero: int):
     mods = load_mods()
     mods[str(member.id)] = numero
     save_mods(mods)
-    await ctx.send(f'Le moderateur {numero} a ete assigne.')
+    await ctx.send(f'✅ Le moderateur {numero} a ete assigne a {member.name}')
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def getmod(ctx, member: discord.Member):
+    mods = load_mods()
+    numero = mods.get(str(member.id))
+    if numero:
+        await ctx.send(f'Le moderateur {numero}')
+    else:
+        await ctx.send(f'Aucun numero assigne.')
 
 TOKEN = os.getenv('TOKEN')
 bot.run(TOKEN)
