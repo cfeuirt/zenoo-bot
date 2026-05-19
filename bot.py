5df79c552c5b2430ff3a50c8150193e1156065b9
cfeuirt <rifireywan25@gmail.com>
2026-05-19 16:01:21 +0000

    fix messages and staff name

--- bot.py
+++ bot.py
@@ -7,8 +7,7 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 MOD_FILE = 'mods.json'
-GUILD_ID = 1506035311048917052
-AUTOROLE_NAME = 'Membres'
+AUTOROLE_FILE = 'autorole.json'
 
 def load_mods():
     if os.path.exists(MOD_FILE):
@@ -20,16 +19,36 @@ def save_mods(mods):
     with open(MOD_FILE, 'w') as f:
         json.dump(mods, f)
 
+def load_autorole():
+    if os.path.exists(AUTOROLE_FILE):
+        with open(AUTOROLE_FILE, 'r') as f:
+            return json.load(f)
+    return {}
+
+def save_autorole(data):
+    with open(AUTOROLE_FILE, 'w') as f:
+        json.dump(data, f)
+
 @bot.event
 async def on_ready():
-    print(f'Bot 1 connecte : {bot.user}')
+    print(f'Bot connecte : {bot.user}')
 
 @bot.event
 async def on_member_join(member):
-    role = discord.utils.get(member.guild.roles, name=AUTOROLE_NAME)
-    if role:
-        await member.add_roles(role)
-        print(f'Role Membres donne a {member.name}')
+    data = load_autorole()
+    role_id = data.get(str(member.guild.id))
+    if role_id:
+        role = member.guild.get_role(int(role_id))
+        if role:
+            await member.add_roles(role)
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def autorole(ctx, role: discord.Role):
+    data = load_autorole()
+    data[str(ctx.guild.id)] = str(role.id)
+    save_autorole(data)
+    await ctx.send(f'✅ Le rôle **{role.name}** sera donné automatiquement aux nouveaux membres !')
 
 @bot.command()
 @commands.has_permissions(administrator=True)
